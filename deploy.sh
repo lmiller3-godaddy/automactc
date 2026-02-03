8191ead2150989359a17a04f78894db54d619098
lmiller3-godaddy <lmiller3@godaddy.com>
2026-02-03 11:35:58 -0500

    restore tar command, whoops

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,9 @@ fmt="json"
 
 pypath="/usr/bin/python"
 
+tar xf $folderName.tar.gz
+cd $folderName
+
 sudo $pypath automactc.py --rtr --prefix $prefix -fmt $fmt -x quicklook coreanalytics safari
 
 mv $prefix*.tar.gz ../.
